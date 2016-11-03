7e7d070c14aa1788e40066fb197dee22b39e670b
KN4CK3R <admin@oldschoolhack.me>
2016-11-03 23:32:51 +0100

    Sync with ReClass.NET code changes.

--- WeakPtrSchemaConverter.cs
+++ WeakPtrSchemaConverter.cs
@@ -7,16 +7,13 @@ using ReClassNET.Nodes;
 
 namespace FrostbitePlugin
 {
-	/// <summary>A schema for the WeakPtrNode.</summary>
-	class WeakPtrSchemaNode : SchemaCustomNode
+	/// <summary>A schema node for the WeakPtrNode.</summary>
+	class WeakPtrSchemaNode : SchemaReferenceNode
 	{
-		public SchemaClassNode InnerNode { get; }
-
 		public WeakPtrSchemaNode(SchemaClassNode inner)
+			: base(SchemaType.Custom, inner)
 		{
 			Contract.Requires(inner != null);
-
-			InnerNode = inner;
 		}
 	}
 
@@ -38,14 +35,14 @@ namespace FrostbitePlugin
 		/// <summary>Checks if the schema can be handled.</summary>
 		/// <param name="schema">The schema to check.</param>
 		/// <returns>True if we can handle schema, false if not.</returns>
-		public bool CanHandleSchema(SchemaCustomNode schema) => schema is WeakPtrSchemaNode;
+		public bool CanHandleSchema(SchemaNode schema) => schema is WeakPtrSchemaNode;
 
 		/// <summary>Creates the schema which represents the node.</summary>
 		/// <param name="node">The node to convert.</param>
 		/// <param name="classes">The mapping from classes to their schema.</param>
 		/// <param name="logger">The logger.</param>
 		/// <returns>The schema which represents the node.</returns>
-		public SchemaCustomNode CreateSchemaFromNode(BaseNode node, IReadOnlyDictionary<ClassNode, SchemaClassNode> classes, ILogger logger)
+		public SchemaNode CreateSchemaFromNode(BaseNode node, IReadOnlyDictionary<ClassNode, SchemaClassNode> classes, ILogger logger)
 		{
 			return new WeakPtrSchemaNode(classes[(node as WeakPtrNode).InnerNode as ClassNode])
 			{
@@ -59,7 +56,7 @@ namespace FrostbitePlugin
 		/// <param name="classes">The mapping from class names to their schema.</param>
 		/// <param name="logger">The logger.</param>
 		/// <returns>The schema which represents the element.</returns>
-		public SchemaCustomNode CreateSchemaFromElement(XElement element, IReadOnlyDictionary<string, SchemaClassNode> classes, ILogger logger)
+		public SchemaNode CreateSchemaFromElement(XElement element, IReadOnlyDictionary<string, SchemaClassNode> classes, ILogger logger)
 		{
 			var reference = element.Attribute(ReClassNetFile.XmlReferenceAttribute)?.Value;
 			if (reference == null || !classes.ContainsKey(reference))
@@ -82,7 +79,7 @@ namespace FrostbitePlugin
 		/// <param name="classes">The mapping from class schemas to their nodes.</param>
 		/// <param name="logger">The logger.</param>
 		/// <returns>The node which is represented by the schema.</returns>
-		public BaseNode CreateNodeFromSchema(SchemaCustomNode schema, IReadOnlyDictionary<SchemaClassNode, ClassNode> classes, ILogger logger)
+		public BaseNode CreateNodeFromSchema(SchemaNode schema, IReadOnlyDictionary<SchemaClassNode, ClassNode> classes, ILogger logger)
 		{
 			var node = new WeakPtrNode
 			{
@@ -97,7 +94,7 @@ namespace FrostbitePlugin
 		/// <param name="schema">The schema to convert.</param>
 		/// <param name="logger">The logger.</param>
 		/// <returns>The element which represents the schema.</returns>
-		public XElement CreateElementFromSchema(SchemaCustomNode schema, ILogger logger)
+		public XElement CreateElementFromSchema(SchemaNode schema, ILogger logger)
 		{
 			return new XElement(
 				ReClassNetFile.XmlNodeElement,
