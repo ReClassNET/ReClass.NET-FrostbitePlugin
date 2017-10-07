3f5d98668d31b5011fe7301941ffb71355dce085
KN4CK3R <admin@oldschoolhack.me>
2017-10-07 17:23:32 +0200

    Fixed changed namespace.

--- WeakPtrNodeConverter.cs
+++ WeakPtrNodeConverter.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
-using ReClassNET.DataExchange;
+using ReClassNET.DataExchange.ReClass;
 using ReClassNET.Logger;
 using ReClassNET.Nodes;
 
