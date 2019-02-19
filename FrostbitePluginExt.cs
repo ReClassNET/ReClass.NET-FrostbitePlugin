b3c81035c6aef023ce57cb86cd95cb4c4228f440
KN4CK3R <admin@oldschoolhack.me>
2019-02-19 15:29:46 +0100

    Removed unneeded code.

--- FrostbitePluginExt.cs
+++ FrostbitePluginExt.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Text;
-using ReClassNET;
-using ReClassNET.Extensions;
-using ReClassNET.Memory;
 using ReClassNET.Nodes;
 using ReClassNET.Plugins;
 
@@ -12,32 +7,7 @@ namespace FrostbitePlugin
 {
 	public class FrostbitePluginExt : Plugin
 	{
-		private IPluginHost host;
-
-		internal static Settings Settings;
-
 		public override Image Icon => Properties.Resources.logo_frostbite;
-
-		public override bool Initialize(IPluginHost host)
-		{
-			//System.Diagnostics.Debugger.Launch();
-
-			if (this.host != null)
-			{
-				Terminate();
-			}
-
-			this.host = host ?? throw new ArgumentNullException(nameof(host));
-
-			Settings = host.Settings;
-
-			return true;
-		}
-
-		public override void Terminate()
-		{
-			host = null;
-		}
 
 		public override IReadOnlyList<INodeInfoReader> GetNodeInfoReaders()
 		{
